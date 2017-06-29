3516f1213eb5253b99266d5c51cf4b9f6f5c7d72
Dario Garcia <dariogarcia@gmail.com>
2017-06-29 14:20:56 +0200

    Added prints and fixed minor typos

--- src/example1_loader.py
+++ src/example1_loader.py
@@ -33,8 +33,10 @@ print 'Found',len(go_gn_links,),'genotype_gene links'
 #find_all_paths(G,G.vs.find(phenotypes[0]).index,G.vs.find(genotypes[0]).index,maxlen=4)
 
 #Compute all alternative paths for connected phenotype-genotype pairs
+#print 'Computing alternative paths among connected pairs'
 #get_connected_phenotype_genotype_alternative_paths(phenotypes, genotypes, genes, ph_ph_links, go_go_links, ph_gn_links, go_gn_links, continuing=True)
 
 #Compute all paths for non-connected phenotype-genotype pairs
-get_disconnected_phenotype_genotype_paths(phenotypes, genotypes, genes, ph_ph_links, go_go_links, ph_gn_links, go_gn_links)
+print 'Computing alternative paths among disconnected pairs'
+get_disconnected_phenotype_genotype_paths(phenotypes, genotypes, genes, ph_ph_links, go_go_links, ph_gn_links, go_gn_links, continuing=True)
 
